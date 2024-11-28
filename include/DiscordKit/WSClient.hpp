16474b3be9bba9407affb0fc3d5a917c23c9e854
Käck Nikolai - Tycho Braheskolan <nikolai.kack@skola.helsingborg.se>
2024-11-28 13:27:48 +0100

    Wrote setGatewayURL_ method (WSClient)

--- include/DiscordKit/WSClient.hpp
+++ include/DiscordKit/WSClient.hpp
@@ -9,8 +9,13 @@
 
 #pragma once
 
-#include <boost/beast/websocket.hpp>
-#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <iostream>
+#include <nlohmann/json.hpp>
 
 
 /**
@@ -21,7 +26,7 @@
 class WSClient {
 public:
 	explicit WSClient(const std::string* token) : token_(*token) {
-		this->setGatewayURL_(token_);
+		this->setGatewayURL_();
 	};
 
 	/** @brief Connects to the WebSocket server. */
@@ -61,11 +66,42 @@ private:
 	void HandleError_();
 
 	/** @brief Sets the gateway URL using the provided token */
-	void setGatewayURL_(std::string& token);
+	void setGatewayURL_() {
+		try {
+		boost::asio::io_context ioc;
+		boost::asio::ssl::context ssl_ctx(boost::asio::ssl::context::sslv23_client);
 
-	boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws_con_;
-	boost::asio::io_context ioc_{};
-	boost::asio::ip::tcp::resolver resolver_{ioc_};
+		boost::asio::ip::tcp::resolver resolver{ioc};
+		auto const endpoint = resolver.resolve("discord.com", "https")
+
+		boost::beast::tcp_stream stream{ioc};
+		stream.connect(endpoint);
+
+		boost::beast::ssl_stream<boost::beast::tcp_stream> ssl_stream(std::move(stream), ssl_ctx);
+		ssl_stream.handshake(boost::asio::ssl::stream_base::client);
+
+		boost::beast::http::request<boost::beast::http::string_body> req{
+    	boost::beast::http::verb::get, "/api/v10/gateway", 11};
+		req.set(boost::beast::http::field::host, "discord.com");
+		req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+		boost::beast::http::write(ssl_stream, req);
+
+		boost::beast::flat_buffer buffer;
+		boost::beast::http::response<boost::beast::http::string_body> res;
+
+		boost::beast::http::read(ssl_stream, buffer, res);
+		std::cout << res << std::endl;
+
+		ssl_stream.shutdown();
+
+		this->gateway_url_ = json.parse(res)["url"];
+		} catch (const std::exception& e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+
+		return 0;
+	};
 
 	std::string token_;
 	std::string gateway_url_;
@@ -73,4 +109,4 @@ private:
 };
 
 
-}
+
